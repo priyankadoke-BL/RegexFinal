91f274d5f17f6f4e0fb800761cfede8ffa2b484b
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-10-09 09:01:06 +0530

    Initial commit

---
+++ src/JavaRegex/RegestrationMain.java
@@ -0,0 +1,14 @@
+package JavaRegex;
+
+public class RegestrationMain {
+
+
+    public static void main(String[] args) {
+        Verify verify = new Verify();
+        System.out.println("Enter the first name");
+        String fName = verify.validName();
+        System.out.println("Enter the last name");
+        String lName = verify.validName();
+    }
+}
+
